fb9f3f12640ed9e3d14b7586d112b3bde125567b
lpf696 <lpf696696@163.com>
2018-07-27 14:05:16 +0800

    【开发】资源目录提交

--- KingOfGun/assets/script/module/common/Singleton.js
+++ KingOfGun/assets/script/module/common/Singleton.js
@@ -1,11 +1,13 @@
+const BaseLoader = require('BaseLoader')
+
 var Singleton = cc.Class({
     ctor: function () {
-
+        
     },
 
     statics: {
         init: function () {
-            
+            Singleton.BaseLoader = new BaseLoader();
         }
     }
 })
