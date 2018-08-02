c126b6dff96d08a4f41d9d700213fb06473937bb
lpf696 <lpf696696@163.com>
2018-08-02 19:58:39 +0800

    【开发】历史积分存储

--- KingOfGun/assets/script/util/Common.js
+++ KingOfGun/assets/script/util/Common.js
@@ -16,6 +16,16 @@ var Common = cc.Class({
             var result = cc.sys.localStorage.getItem('historyScore');
             if(result == null) {
                 cc.sys.localStorage.setItem('historyScore', 0);
+                if(CC_WECHATGAME) {
+                    var kvDataList = new Array();
+                    kvDataList.push({
+                        key: historyScore,
+                        value: "0"
+                    });
+                    wx.setUserCloudStorage({
+                        KVDataList: kvDataList
+                    })
+                }
                 return 0;
             }
             return result;
@@ -25,6 +35,16 @@ var Common = cc.Class({
             var result = cc.sys.localStorage.getItem('historyScore');
             if(scoreNum > result) {
                cc.sys.localStorage.setItem('historyScore', scoreNum);
+               if(CC_WECHATGAME) {
+                var kvDataList = new Array();
+                kvDataList.push({
+                    key: historyScore,
+                    value: scoreNum
+                });
+                wx.setUserCloudStorage({
+                    KVDataList: kvDataList
+                })
+            }
            }
         },
     }
