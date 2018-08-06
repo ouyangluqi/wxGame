07830872cc633421d97af35008449aa94b1d7144
lpf696 <lpf696696@163.com>
2018-08-06 17:03:51 +0800

    【开发】打印信息增加环境判断

--- KingOfGun/assets/script/util/Log.js
+++ KingOfGun/assets/script/util/Log.js
@@ -5,12 +5,16 @@ var Log = cc.Class({
 
     statics: {
         logD: function (message) {
-            console.log("[INFO]"+message)
+            if(!CC_DEBUG) {
+                console.log("[INFO]"+message)
+            }
             cc.info(message)
         },
 
         logE: function (message) {
-            console.log("[ERROR]"+message)
+            if(!CC_DEBUG) {
+                console.log("[ERROR]"+message)
+            }
             cc.error(message)
         }
     }
